28280da6bbebfdc45b93f8386620977aa39cc504
Iñaki <izabala123@gmail.com>
2022-07-23 17:12:43 +0200

    Integration with U++

--- Eigen/ToStringPlugin.h
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2021 - 2022, the Anboto author and contributors
-	
-UPP::String ToString() const {
-	std::stringstream s;
-	s << *this;
-	return s.str();
-}
-
--- STEM4U/srcimp.tpp/all.i
+++
@@ -1,4 +0,0 @@
-TOPIC("ArbitraryPrecision_en-us")
-#include "ArbitraryPrecision_en-us.tppi"
-END_TOPIC
-
