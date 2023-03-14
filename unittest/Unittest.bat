abebfd77ebe1801f7728891c0df56e5a55dbc914
Iñaki <izabala123@gmail.com>
2023-03-14 07:14:44 +0100

    Replaced MSVS2017 with 2022

--- unittest/Unittest.bat
+++ unittest/Unittest.bat
@@ -1,7 +1,7 @@
 @echo "Testing Anboto for CLANGX64"
 call UnittestAnboto.bat CLANGX64
 @IF %ERRORLEVEL% NEQ 0 PAUSE "Error testing Anboto CLANG"
-@echo "Testing Anboto for MSVS17x64"
-call UnittestAnboto.bat MSVS17x64
+@echo "Testing Anboto for MSVS22x64"
+call UnittestAnboto.bat MSVS22x64
 @IF %ERRORLEVEL% NEQ 0 PAUSE "Error testing Anboto MSVS17x64"
 
