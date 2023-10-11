f236f1494aafb100191acc293bd50282384747b6
Iñaki <izabala123@gmail.com>
2023-10-11 19:05:16 +0200

    Removed compiling with MSVC

--- unittest/Unittest.bat
+++ unittest/Unittest.bat
@@ -2,6 +2,6 @@
 call UnittestAnboto.bat CLANGX64
 @IF %ERRORLEVEL% NEQ 0 PAUSE "Error testing Anboto CLANG"
 @echo "Testing Anboto for MSVS22x64"
-call UnittestAnboto.bat MSVS22x64
-@IF %ERRORLEVEL% NEQ 0 PAUSE "Error testing Anboto MSVS17x64"
+@echo call UnittestAnboto.bat MSVS22x64
+@echo IF %ERRORLEVEL% NEQ 0 PAUSE "Error testing Anboto MSVS17x64"
 
