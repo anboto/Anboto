6a2b99962afb31614134a8554d3aecdfc7805f5e
Iñaki <izabala123@gmail.com>
2024-08-16 11:39:36 +0200

    Fixed unitest

--- unittest/UnittestAnboto.bat
+++ unittest/UnittestAnboto.bat
@@ -3,68 +3,70 @@ md .test
 del .test\*.* /q
 
 umk Anboto examples/Functions4U_demo_cl %1 -r   .\.test\Functions4U_Demo.exe
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem compiling Functions4U_demo_cl"
 .\.test\Functions4U_Demo.exe	
-@IF %ERRORLEVEL% NEQ 0  EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem testing Functions4U_demo_cl"
 
 umk Anboto examples/SysInfo_demo_cl %1 -r   .\.test\SysInfo_demo_console.exe
-@IF %ERRORLEVEL% NEQ 0  EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem compiling SysInfo_demo_cl"
 .\.test\SysInfo_demo_console.exe	
-@IF %ERRORLEVEL% NEQ 0  EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem testing SysInfo_demo_cl"
 
 umk Anboto examples/STEM4U_demo_test_cl %1 -r   .\.test\STEM4U_Demo.exe
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem compiling STEM4U_demo_test_cl"
 .\.test\STEM4U_Demo.exe -test	
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem testing STEM4U_demo_test_cl"
 
 umk Anboto examples/Eigen_demo_cl %1 -r   .\.test\Eigen_Demo.exe
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem compiling Eigen_demo_cl"
 .\.test\Eigen_Demo.exe	
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem testing Eigen_demo_cl"
 
 umk Anboto examples/ScatterDraw_demo_cl %1 -r   .\.test\ScatterDraw_Demo.exe
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem compiling ScatterDraw_demo_cl"
 .\.test\ScatterDraw_Demo.exe	
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem testing ScatterDraw_demo_cl"
 
 umk Anboto examples/OfficeAutomation_demo_cl %1 -r   .\.test\OfficeAutomation_demo_console.exe
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem compiling OfficeAutomation_demo_cl"
 
 umk Anboto examples/ScatterCtrl_demo %1 -r +GUI  .\.test\ScatterCtrl_Demo.exe
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem compiling ScatterCtrl_demo"
 
 umk Anboto examples/Surface_demo_test_cl %1 -r   .\.test\Surface_demo_test_cl.exe
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem compiling Surface_demo_test_cl"
 
 umk Anboto examples/SysInfo_demo_cl %1 -r   .\.test\SysInfo_demo_cl.exe
-@IF %ERRORLEVEL% NEQ 0  EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0  PAUSE "Problem compiling SysInfo_demo_cl"
 
 umk Anboto examples/SysInfo_demo_gui %1 -r +GUI  .\.test\SysInfo_demo_gui.exe
-@IF %ERRORLEVEL% NEQ 0  EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0  PAUSE "Problem compiling SysInfo_demo_gui"
 
 umk Anboto examples/Controls4U_demo %1 -r +GUI  .\.test\Controls4U_demo.exe
-@IF %ERRORLEVEL% NEQ 0  EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0  PAUSE "Problem compiling Controls4U_demo"
 
 umk Anboto examples/GLCanvas_demo %1 -r +GUI  .\.test\GLCanvas_demo.exe
-@IF %ERRORLEVEL% NEQ 0  EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0  PAUSE "Problem compiling GLCanvas_demo"
 
 umk Anboto examples/MatIO_demo_cl %1 -r   .\.test\MatIO_demo_cl.exe
-@IF %ERRORLEVEL% NEQ 0  EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0  PAUSE "Problem compiling MatIO_demo_cl"
 cd .\.test
 MatIO_demo_cl.exe -test	
+@IF %ERRORLEVEL% NEQ 0  PAUSE "Problem testing MatIO_demo_cl"
 cd ..
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem"
 
 umk Anboto examples/Hdf5_demo_cl %1 -r   .\.test\Hdf5_demo_cl.exe
-@IF %ERRORLEVEL% NEQ 0  EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0  PAUSE "Problem compiling Hdf5_demo_cl"
 cd .\.test
 Hdf5_demo_cl.exe -test	
 cd ..
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem"
 
 umk Anboto examples/NetCDF_demo_cl %1 -r   .\.test\NetCDF_demo_cl.exe
-@IF %ERRORLEVEL% NEQ 0  EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0  PAUSE "Problem compiling NetCDF_demo_cl"
 cd .\.test
 NetCDF_demo_cl.exe -test	
+@IF %ERRORLEVEL% NEQ 0  PAUSE "Problem testing NetCDF_demo_cl"
 cd ..
-@IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+@IF %ERRORLEVEL% NEQ 0 PAUSE "Problem "
--- unittest/UnittestAnboto.sh
+++ unittest/UnittestAnboto.sh
@@ -1,44 +1,76 @@
 #!/bin/bash
 set -x
 set -e
+export PATH=$PATH:/home/aupa/upp
 
 cwd=$(pwd)
 
-umk Anboto examples/Functions4U_demo_cl CLANG -r +SHARED  $cwd/.test/Functions4U_DemoCLANG
+umk Anboto examples/Functions4U_demo_cl CLANG    -r +SHARED  $cwd/.test/Functions4U_DemoCLANG
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 $cwd/.test/Functions4U_DemoCLANG	
-umk Anboto examples/Functions4U_demo_cl GCC -r +SHARED  $cwd/.test/Functions4U_DemoGCC	
-$cwd/.test/Functions4U_DemoGCC	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+umk Anboto examples/Functions4U_demo_cl CLANG_17 -r +SHARED  $cwd/.test/Functions4U_DemoCLANG_17	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+$cwd/.test/Functions4U_DemoCLANG_17	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 
-umk Anboto examples/SysInfo_demo_cl CLANG  -r  +SHARED $cwd/.test/SysInfo_demo_consoleCLANG
+umk Anboto examples/SysInfo_demo_cl CLANG    -r  +SHARED $cwd/.test/SysInfo_demo_consoleCLANG
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 $cwd/.test/SysInfo_demo_consoleCLANG	
-umk Anboto examples/SysInfo_demo_cl GCC -r  +SHARED $cwd/.test/SysInfo_demo_consoleGCC	
-$cwd/.test/SysInfo_demo_consoleGCC	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+umk Anboto examples/SysInfo_demo_cl CLANG_17 -r  +SHARED $cwd/.test/SysInfo_demo_consoleCLANG_17	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+$cwd/.test/SysInfo_demo_consoleCLANG_17	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 
-umk Anboto examples/STEM4U_demo_test_cl CLANG  -r  +SHARED $cwd/.test/STEM4U_DemoCLANGx64
-$cwd/.test/STEM4U_DemoCLANGx64 -test	
-umk Anboto examples/STEM4U_demo_test_cl GCC -r +SHARED  $cwd/.test/STEM4U_DemoMSBT19x64	
-$cwd/.test/STEM4U_DemoMSBT19x64 -test
+umk Anboto examples/STEM4U_demo_test_cl CLANG    -r  +SHARED $cwd/.test/STEM4U_DemoCLANG
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+$cwd/.test/STEM4U_DemoCLANG -test	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+umk Anboto examples/STEM4U_demo_test_cl CLANG_17 -r +SHARED  $cwd/.test/STEM4U_DemoCLANG_17
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+$cwd/.test/STEM4U_DemoCLANG_17 -test
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 
-umk Anboto examples/Eigen_demo_cl CLANG  -r  +SHARED $cwd/.test/Eigen_DemoCLANGx64
-$cwd/.test/Eigen_DemoCLANGx64	
-umk Anboto examples/Eigen_demo_cl GCC -r  +SHARED $cwd/.test/Eigen_DemoMSBT19x64	
-$cwd/.test/Eigen_DemoMSBT19x64	
+umk Anboto examples/Eigen_demo_cl CLANG    -r  +SHARED $cwd/.test/Eigen_DemoCLANG
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+$cwd/.test/Eigen_DemoCLANG	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+umk Anboto examples/Eigen_demo_cl CLANG_17 -r  +SHARED $cwd/.test/Eigen_DemoCLANG_17
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+$cwd/.test/Eigen_DemoCLANG_17	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 
-umk Anboto examples/ScatterDraw_demo_cl CLANG  -r +SHARED  $cwd/.test/ScatterDraw_DemoCLANGx64
-$cwd/.test/ScatterDraw_DemoCLANGx64	
-umk Anboto examples/ScatterDraw_demo_cl GCC -r +SHARED  $cwd/.test/ScatterDraw_DemoMSBT19x64
-$cwd/.test/ScatterDraw_DemoMSBT19x64	
+umk Anboto examples/ScatterDraw_demo_cl CLANG  -r +SHARED  $cwd/.test/ScatterDraw_DemoCLANG
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+$cwd/.test/ScatterDraw_DemoCLANG	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+umk Anboto examples/ScatterDraw_demo_cl CLANG_17 -r +SHARED  $cwd/.test/ScatterDraw_DemoCLANG_17
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+$cwd/.test/ScatterDraw_DemoCLANG_17	
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 
 umk Anboto examples/Hdf5_demo_cl CLANG -r +SHARED  $cwd/.test/Hdf5_DemoCLANG
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 $cwd/.test/Hdf5_DemoCLANG
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+umk Anboto examples/Hdf5_demo_cl CLANG_17 -r +SHARED  $cwd/.test/Hdf5_DemoCLANG_17
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+$cwd/.test/Hdf5_DemoCLANG_17
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 
 umk Anboto examples/NetCDF_demo_cl CLANG -r +SHARED  $cwd/.test/NetCDF_DemoCLANG
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 $cwd/.test/NetCDF_DemoCLANG
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+umk Anboto examples/NetCDF_demo_cl CLANG_17 -r +SHARED  $cwd/.test/NetCDF_DemoCLANG_17
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+$cwd/.test/NetCDF_DemoCLANG_17
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 
-umk Anboto examples/OfficeAutomation_demo_cl CLANG  -r  +SHARED $cwd/.test/OfficeAutomation_demo_consoleCLANGx64
-umk Anboto examples/OfficeAutomation_demo_cl GCC -r +SHARED $cwd/.test/OfficeAutomation_demo_consoleMSBT19x64
+umk Anboto examples/ScatterCtrl_demo CLANG  -r  +GUI,SHARED $cwd/.test/ScatterCtrl_DemoCLANG
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
+umk Anboto examples/ScatterCtrl_demo CLANG_17 -r  +GUI,SHARED $cwd/.test/ScatterCtrl_DemoCLANG_17
+[ $? -ne 0 ] && { echo "Problem found"; read -p "Press Enter to continue..."; }
 
-umk Anboto examples/ScatterCtrl_demo CLANG  -r  +GUI,SHARED $cwd/.test/ScatterCtrl_DemoCLANGx64
-umk Anboto examples/ScatterCtrl_demo GCC -r  +GUI,SHARED $cwd/.test/ScatterCtrl_DemoMSBT19x64
-
-echo -----------All done. NO error!
+exit
