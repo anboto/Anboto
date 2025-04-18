8f935ffc5b37781f520aaab1dba519650ca7e4a9
Iñaki <izabala123@gmail.com>
2025-04-18 21:05:38 +0200

    Added MSVS22x64

--- unittest/Unittest.bat
+++ unittest/Unittest.bat
@@ -7,7 +7,13 @@
 @title "Testing Anboto for CLANGX64
 call UnittestAnboto.bat CLANGX64
 @IF %ERRORLEVEL% NEQ 0 PAUSE "Error testing Anboto CLANG"
-@echo "Testing Anboto for MSVS22x64"
-@echo call UnittestAnboto.bat MSVS22x64
-@echo IF %ERRORLEVEL% NEQ 0 PAUSE "Error testing Anboto MSVS17x64"
+
+set "prefix=CPU-C"
+set "name=%COMPUTERNAME%"
+set "first5=%name:~0,5%"
+if /I not "%first5%"=="%prefix%" (
+	echo "Testing Anboto for MSVS22x64"
+	call UnittestAnboto.bat MSVS22x64
+	IF %ERRORLEVEL% NEQ 0 PAUSE "Error testing Anboto MSVS17x64"
+)
 
